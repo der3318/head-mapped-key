eebc7c475b060a380ca85ec2152529b8500a5dc0
Der Chien <der3318@yahoo.com.tw>
2023-10-22 14:31:58 +0800

    20231022 jna for better CTRL+C handling

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,12 +2,35 @@ import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
 import java.util.*;
+import org.apache.logging.log4j.*;
+import org.jline.reader.*;
+import org.jline.terminal.*;
 
 public class App {
+
+    private static final Logger LOGGER = LogManager.getLogger();
 
     public static void keyPressAndRelease(Robot robot, int key) {
         robot.keyPress(key);
         robot.keyRelease(key);
+    }
+
+    public static void tryWaitForUserInterruptionToShutdown() throws IOException {
+        Terminal terminal = TerminalBuilder.builder().jna(true).build();
+        LineReader consoleLineReader = LineReaderBuilder.builder().terminal(terminal).build();
+        while (true) {
+            try {
+                consoleLineReader.readLine("");
+            } catch (UserInterruptException e) {
+                // hoping to catch CTRL+C with the help of JNA here
+                LOGGER.info("Shutting Down");
+                Runtime.getRuntime().exit(0);
+            } catch (EndOfFileException e) {
+                // dumb terminals like `gradle run` can not hook CTRL+C and may not have STDIN
+                LOGGER.info("STDIN Unavailable");
+                break;
+            }
+        }
     }
 
     public static void main(String[] args) throws IOException, AWTException, IllegalAccessException, NoSuchFieldException {
@@ -28,5 +51,6 @@ public class App {
         headPoseListener.onHeadTurnedRight(() -> keyPressAndRelease(robot, keyOnHeadTurnedRight));
         Runtime.getRuntime().addShutdownHook(new Thread(() -> headPoseListener.stopMonitoring()));
         headPoseListener.startMonitoring();
+        tryWaitForUserInterruptionToShutdown();
     }
 }
